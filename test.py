f2097834b61680f027282ff0bda7f3be05b0a10f
kariaki58 <programmerjavascript901@gmail.com>
2024-01-29 17:08:18 +0100

    added routes and test file

--- test.py
+++ test.py
@@ -5,29 +5,22 @@ from musicapp.models.user import User
 from musicapp.models.comment import Comment
 from musicapp.models.favourite import Favourite
 from musicapp.models.playlist import Playlist
+from run import app
 
 
-from musicapp.run import app
-
-
-<<<<<<< HEAD
 if app:
-    user = User(email="jjjfda2@gmaddskl.c", password="132sdfa32", username="cfdory2")
-=======
-if  app:
-    user = User(email="jjj@gmaddskl.c", password="1323", username="cory")
->>>>>>> feature_home
+    user = User.query.first()
+    song = Song.query.offset(1).first()
+    # song = Song(title="fire masfdanxxy2", artist_name="asndrewfds2 tate v2", song_path="~/hofdsaf2me/dirfd", user=user)
+    # user_q = User.query.all()
+    print(song.id)
+    comment = Comment(text="Hello world", user_id=user.id, song_id=song.id)
+    # playlist = Playlist(title="My2 bessdft music", user=user, songs=song)
 
-    song = Song(title="fire masfdanxxy2", artist_name="asndrewfds2 tate v2", song_path="~/hofdsaf2me/dirfd", user=user)
-    user_q = User.query.all()
-
-    comment = Comment(text="hey2 sdsafi hate you favour", user=user, song=song)
-    playlist = Playlist(title="My2 bessdft music", user=user, songs=song)
-
-    database.session.add(user)
-    database.session.add(song)
+    # database.session.add(user)
+    # database.session.add(song)
     database.session.add(comment)
-    database.session.add(playlist)
+    # database.session.add(playlist)
 
     print("added...")
     database.session.commit()
